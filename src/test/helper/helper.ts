37e3ff08139e1969f75fd058e2610d816e88f810
User <user@192-168-1-115.tpgi.com.au>
2020-03-08 23:35:22 +1100

    Adding Modularisation to function- Manish

--- src/test/helper/helper.ts
+++ src/test/helper/helper.ts
@@ -1,6 +1,12 @@
 import fetch from 'node-fetch';
 
-export async function get(url: string) {
-    const request =  await fetch(`${url}`);
+export async function PerformAPI(url: string , method: string , body?: string) {
+    const headers = { 'Content-Type': 'application/json' };
+    const request = await fetch(`${url}`, {
+        method: method,
+        headers: headers,
+        body: body
+    });
     return request;
 }
+
--- src/test/testCases/testCaseOne.test.ts
+++ src/test/testCases/testCaseOne.test.ts
@@ -1,11 +1,11 @@
-import { get } from "../helper/helper";
+import { PerformAPI } from "../helper/helper";
 import { GitResponse } from "../types/types";
 
 
 describe('Testing the node fetch Get', () => {
 
     it('Validate the get function', async () => {
-        const getcall = get('https://api.github.com/users/github');
+        const getcall = PerformAPI('https://api.github.com/users/github' , 'GET');
         const resposneJson: GitResponse = await getcall.then(res=>res.json());
         expect(resposneJson.avatar_url).toEqual('https://avatars1.githubusercontent.com/u/9919?v=4');
         const status = await getcall.then(res=>res.status);
